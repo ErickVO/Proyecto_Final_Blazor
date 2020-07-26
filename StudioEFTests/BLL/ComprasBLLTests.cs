0382b4e28685c875b291f33f6f6aab6f9c24891f
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-07-25 23:02:01 -0400

    Test ComprasBLL

---
+++ StudioEFTests/BLL/ComprasBLLTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudioEF.BLL;
+using StudioEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioEF.BLL.Tests
+{
+    [TestClass()]
+    public class ComprasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso = false;
+
+            Compras compras = new Compras();
+            compras.CompraId = 0;
+            compras.UsuarioId = 1;
+            compras.Monto = 0;
+            compras.ComprasDetalle.Add(new ComprasDetalle
+            {
+                ComprasDetalleId = 0,
+                CompraId = compras.CompraId,
+                ArticuloId = 1,
+                Descripcion = ArticulosBLL.ObtenerDescripcion(1),
+                CantidadArticulos = 2
+            }) ;
+
+            Assert.IsTrue(ComprasBLL.Guardar(compras));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            paso = ComprasBLL.Eliminar(1);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Compras compras;
+            compras = ComprasBLL.Buscar(1);
+            Assert.IsNotNull(compras);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            var lista = new List<Compras>();
+            lista = ComprasBLL.GetList(p => true);
+            Assert.IsNotNull(lista);
+        }
+    }
+}
