6aae740f0ed1f044e3f524ca4aa21f33cec6e7a2
Frandy Francisco Mota Rodriguez <frandy_mota@ucne.edu.do>
2020-07-27 23:27:43 -0500

    Creando Test

---
+++ StudioEFTests/BLL/EventosBLLTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudioEF.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioEF.BLL.Tests
+{
+    [TestClass()]
+    public class EventosBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+      
+    }
+}
---
+++ StudioEFTests/BLL/VentasBLLTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudioEF.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioEF.BLL.Tests
+{
+    [TestClass()]
+    public class VentasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }        
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
